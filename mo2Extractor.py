3145eb94c36a05a5ed1e9cfd6599e0fe0ee1aa55
VeryFunnyMonkey <github@chimpzone.net>
2024-03-21 16:29:40 +0800

    added the ability to enable and disable mods

--- mo2Extractor.py
+++ mo2Extractor.py
@@ -11,34 +11,39 @@ def read_mod_list(mod_list_file):
     mod_list.reverse()
     return mod_list
 
-def set_mod_folder(mod_list_file, output_elem):
+def set_mod_folder(mod_list_file):
     mods_folder = mod_list_file.split("/profiles")[0] + "/mods"
 
     if not os.path.exists(mods_folder):
         mods_folder = sg.popup_get_folder("Mods folder not found! Please specify the mods folder.")
         
-    output_elem.update(value ="Mods Folder: \n" +  mods_folder)
+    print("Mods Folder: \n" +  mods_folder)
     return mods_folder
 
-def copy_mod_list(mods_folder, mod_list, destination, output_elem):
+def copy_mod_list(mods_folder, mod_list, destination, enabled_mods_elem):
     mod_files = os.listdir(mods_folder)
 
     if os.path.exists(destination):
         shutil.rmtree(destination)
 
-    output = ""
+    enabled_mods = enabled_mods_elem.get_list_values()
     for mod in mod_list:
-        if mod in mod_files:
-            output += "Copying mod: " + mod + "\n"
+        if mod in mod_files and mod in enabled_mods:
+            print( "Copying mod: " + mod + "\n")
             shutil.copytree((mods_folder + "/" + mod), destination, dirs_exist_ok=True)
-            output_elem.update(value=output)
-    sg.popup("Mod list copied successfully!")
+    print("Mod list copied successfully!\n")
+    # sg.popup("Mod list copied successfully!") -- Need to call it in the main thread, workaround is to update the output.
 
 
-def copy_mods_thread(mod_list_file, mod_list, destination, output_elem):
-    t = threading.Thread(target=copy_mod_list, args=(mod_list_file, mod_list, destination, output_elem))
+def copy_mods_thread(mod_list_file, mod_list, destination, enabled_mods_elem):
+    t = threading.Thread(target=copy_mod_list, args=(mod_list_file, mod_list, destination, enabled_mods_elem))
     t.daemon = True  # Set the thread as daemon - kills the thread on window close
     t.start()
+
+
+def update_mods_list(mod_list, enabled_mods_elem, disabled_mods_elem):
+    enabled_mods_elem.update(values=mod_list)
+    disabled_mods_elem.update(values=[])
 
 
 def main():
@@ -55,11 +60,21 @@ def main():
     ]
 
     tab2_layout = [
-        [sg.Text("Enter a source first.", key="-FILTERTEXT-")
+        [
+            sg.Text("Enter a source first.", key="-FILTERTEXT1-")
         ],
+        [
+            sg.Listbox(values=[], size=(60, 10), key="-ENABLEDMODS-", enable_events=True, select_mode="LISTBOX_SELECT_MODE_SINGLE")
+        ],
+        [
+            sg.Text("Enter a source first.", key="-FILTERTEXT2-")
+        ],
+        [
+            sg.Listbox(values=[], size=(60, 10), key="-DISABLEDMODS-",enable_events=True, select_mode="LISTBOX_SELECT_MODE_SINGLE")
+        ]
     ]
 
-    layout = [[sg.TabGroup([[sg.Tab("Copy Mods", tab1_layout), sg.Tab("Filter Mods", tab2_layout)]])]]
+    layout = [[sg.TabGroup([[sg.Tab("Copy Mods", tab1_layout), sg.Tab("Mod List", tab2_layout)]])]]
 
     window = sg.Window("Mod Copy Tool", layout)
 
@@ -69,14 +84,31 @@ def main():
             break
         if event=="source":
             source = values["source"]
-            modList = read_mod_list(source)
-            mods_folder = set_mod_folder(source, window["-OUTPUT-"])
-            window["-FILTERTEXT-"].update("Toggle Mods to be copied")
+            mod_list = read_mod_list(source)
+            enabled_mods = mod_list
+            disabled_mods = []
+            mods_folder = set_mod_folder(source)
+            window["-FILTERTEXT1-"].update("Enabled Mods:")
+            window["-FILTERTEXT2-"].update("Disabled Mods:")
+            update_mods_list(mod_list, window["-ENABLEDMODS-"], window["-DISABLEDMODS-"])
         if event=="destination":
             destination = values["destination"]
+        if event=="-ENABLEDMODS-" and enabled_mods:
+            selected_mod = values["-ENABLEDMODS-"][0]
+            enabled_mods.remove(selected_mod)
+            disabled_mods.append(selected_mod)
+            window["-ENABLEDMODS-"].update(values=enabled_mods)
+            window["-DISABLEDMODS-"].update(values=disabled_mods)
+        if event=="-DISABLEDMODS-" and disabled_mods:
+            selected_mod = values["-DISABLEDMODS-"][0]
+            disabled_mods.remove(selected_mod)
+            enabled_mods.append(selected_mod)
+            window["-DISABLEDMODS-"].update(values=disabled_mods)
+            window["-ENABLEDMODS-"].update(values=enabled_mods)
+
         if event == "Go":
             if source and destination:
-                copy_mods_thread(mods_folder, modList, destination, window["-OUTPUT-"])
+                copy_mods_thread(mods_folder, mod_list, destination, window["-ENABLEDMODS-"])
 
     window.close()
 
