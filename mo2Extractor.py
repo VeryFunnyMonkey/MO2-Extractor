bfa76840146d68440dc89f3ab451069127303065
VeryFunnyMonkey <github@chimpzone.net>
2024-03-21 14:30:55 +0800

    added checking if mod folder path is correct

--- mo2Extractor.py
+++ mo2Extractor.py
@@ -11,22 +11,28 @@ def read_mod_list(mod_list_file):
     mod_list.reverse()
     return mod_list
 
+def set_mod_folder(mod_list_file, output_elem):
+    mods_folder = mod_list_file.split("/profiles")[0] + "/mods"
 
-def copy_mod_list(mod_list_file, mod_list, destination, output_elem):
-    modsFolder = mod_list_file.split("/profiles")[0] + "/mods"
+    if not os.path.exists(mods_folder):
+        mods_folder = sg.popup_get_folder("Mods folder not found! Please specify the mods folder.")
+        
+    output_elem.update(value ="Mods Folder: \n" +  mods_folder)
+    return mods_folder
 
-    modFiles = os.listdir(modsFolder)
+def copy_mod_list(mods_folder, mod_list, destination, output_elem):
+    mod_files = os.listdir(mods_folder)
 
     if os.path.exists(destination):
         shutil.rmtree(destination)
 
     output = ""
     for mod in mod_list:
-        if mod in modFiles:
+        if mod in mod_files:
             output += "Copying mod: " + mod + "\n"
-            shutil.copytree((modsFolder + "/" + mod), destination, dirs_exist_ok=True)
+            shutil.copytree((mods_folder + "/" + mod), destination, dirs_exist_ok=True)
             output_elem.update(value=output)
-    sg.popup("Mod list copying started successfully!")
+    sg.popup("Mod list copied successfully!")
 
 
 def copy_mods_thread(mod_list_file, mod_list, destination, output_elem):
@@ -39,10 +45,10 @@ def main():
     layout = [
         [
             sg.Text("Source"),
-            sg.InputText(key="source"),
+            sg.InputText(key="source", enable_events=True),
             sg.FileBrowse(file_types=(("Modlist Files", "modlist.txt"),)),
         ],
-        [sg.Text("Destination"), sg.InputText(key="destination"), sg.FolderBrowse()],
+        [sg.Text("Destination"), sg.InputText(key="destination", enable_events=True), sg.FolderBrowse()],
         [sg.Button("Go")],
         [sg.Text("Output", size=(10, 1))],
         [sg.Output(size=(60, 10), key="-OUTPUT-", font="Courier 10")],
@@ -54,12 +60,15 @@ def main():
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
             break
-        if event == "Go":
+        if event=="source":
             source = values["source"]
+            modList = read_mod_list(source)
+            mods_folder = set_mod_folder(source, window["-OUTPUT-"])
+        if event=="destination":
             destination = values["destination"]
+        if event == "Go":
             if source and destination:
-                modList = read_mod_list(source)
-                copy_mods_thread(source, modList, destination, window["-OUTPUT-"])
+                copy_mods_thread(mods_folder, modList, destination, window["-OUTPUT-"])
 
     window.close()
 
